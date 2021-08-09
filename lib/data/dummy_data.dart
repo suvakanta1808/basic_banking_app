217b07227d5d5782df57df30480dc5b85f69c994
suvakanta1808 <58504654+suvakanta1808@users.noreply.github.com>
2021-08-09 09:28:43 +0530

    added 10 dummy users

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -3,29 +3,54 @@ import 'package:bank_app/models/user.dart';
 class DummyData {
   List<User> _userData = [
     User(
-      userName: 'Surya Singh',
+      userName: 'Suryabeer Singh',
       email: 'surya@gmail.com',
       balance: 12475.00,
     ),
     User(
-      userName: 'Amit Singh',
+      userName: 'Amit Vansal',
       email: 'amit@gmail.com',
-      balance: 12475.00,
+      balance: 10360.00,
     ),
     User(
-      userName: 'Ajit Singh',
+      userName: 'Ajit Thakur',
       email: 'ajit@gmail.com',
-      balance: 12475.00,
+      balance: 5670.00,
     ),
     User(
-      userName: 'Priya Singh',
+      userName: 'Priya Patnaik',
       email: 'priya@gmail.com',
-      balance: 12475.00,
+      balance: 22400.00,
     ),
     User(
-      userName: 'Arya Singh',
-      email: 'arya@gmail.com',
-      balance: 12475.00,
+      userName: 'Komlpreet Roy',
+      email: 'komlpreet@gmail.com',
+      balance: 1200.00,
+    ),
+    User(
+      userName: 'Jeet Patel',
+      email: 'jeet@gmail.com',
+      balance: 17840.00,
+    ),
+    User(
+      userName: 'Ganesh Sahoo',
+      email: 'ganesh@gmail.com',
+      balance: 15390.00,
+    ),
+    User(
+      userName: 'Rahul Roy',
+      email: 'rahul@gmail.com',
+      balance: 32000.00,
+    ),
+    User(
+      userName: 'Anil Kumar',
+      email: 'anil@gmail.com',
+      balance: 42000.00,
+    ),
+    User(
+      userName: 'Jagjeet Singh',
+      email: 'jagjeet@gmail.com',
+      balance: 60240.00,
     ),
   ];
 
