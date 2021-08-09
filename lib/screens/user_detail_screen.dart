caa3e580b9b75ef02d2f482233ec3ea873d558a2
suvakanta1808 <58504654+suvakanta1808@users.noreply.github.com>
2021-08-09 10:45:01 +0530

    changed UI of the screen

--- lib/screens/user_detail_screen.dart
+++ lib/screens/user_detail_screen.dart
@@ -17,135 +17,136 @@ class UserDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Details'),
+        title: Text('User Profile'),
+        backgroundColor: Colors.blue.shade800,
       ),
       body: Container(
-        height: MediaQuery.of(context).size.height * 0.65,
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadiusDirectional.only(
-            bottomStart: Radius.circular(25),
-            bottomEnd: Radius.circular(25),
-          ),
-          color: Colors.blue,
-        ),
+        color: Colors.white24,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.blue,
-                    ),
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.white,
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 65,
-                    color: Colors.blue.shade700,
-                  ),
-                ),
-                Card(
-                  elevation: 5,
-                  color: Colors.blue.shade700,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)),
-                  child: Container(
-                    padding: EdgeInsets.all(15),
-                    width: 180,
-                    height: 220,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Name:',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          name,
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Email-ID:',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          email,
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Account Balance:',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '$curBalance',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+            Container(
+              color: Colors.white,
+              height: 250,
+              margin: EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.blue.shade800,
+                      child: Icon(
+                        Icons.person,
+                        size: 35,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Divider(
+                    color: Colors.blue.shade800,
+                    indent: 50,
+                    endIndent: 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Username:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        name,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Spartan',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.blue.shade800,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Email Id:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        email,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Spartan',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.blue.shade800,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Current Balance:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        curBalance.toStringAsFixed(2),
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Spartan',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.blue.shade800,
+                  ),
+                ],
+              ),
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 RaisedButton(
-                  elevation: 5,
-                  color: Colors.white38,
+                  color: Colors.amber,
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (ctx) => UserSelectionScreen(User(
-                          userName: name,
-                          email: email,
-                          balance: curBalance,
-                        )),
+                        builder: (ctx) => UserSelectionScreen(
+                          User(
+                            userName: name,
+                            email: email,
+                            balance: curBalance,
+                          ),
+                        ),
                       ),
                     );
                   },
-                  child: Text(
-                    'Transfer Money',
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(13)),
+                  child: Container(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        'Transfer Money',
+                        style: TextStyle(fontSize: 17),
+                      )),
+                  elevation: 0,
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
